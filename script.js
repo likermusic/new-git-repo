2a05be4f6c8a2d0fc7229610097fc7df2fdf6d53
Sergey Timofeev <drumliker@yandex.ru>
2022-04-10 10:08:35 +0300

    Create project

---
+++ script.js
@@ -0,0 +1,17 @@
+//http://jsonplaceholder.typicode.com/
+
+let resp = fetch("http://jsonplaceholder.typicode.com/posts");
+console.log(resp);
+
+setTimeout({
+  ....
+},10000)
+
+
+let a = 45'
+log(a)
+
+
+
+
+
